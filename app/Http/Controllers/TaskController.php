006e0bce89f1c796e380d87f10e437453ec499a1
mourishitz <gabrielmorishita@hotmail,com>
2023-01-17 16:20:57 -0300

    ✨ Criação de Tarefas com envio de e-mail integrado e redirecionamento para rota de show()

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\NewTaskMail;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class TaskController extends Controller
 {
@@ -37,22 +39,24 @@ class TaskController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        $task = Task::create($request->all());
+        Mail::to(auth()->user()->email)->send(new NewTaskMail($task));
+        return redirect()->route('task.show', ['task'=> $task->id]);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return NewTaskMail
      */
     public function show(Task $task)
     {
-        //
+        return $task;
     }
 
     /**
