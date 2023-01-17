44533984dda33c615fcc6b4241285c391d7f240e
mourishitz <gabrielmorishita@hotmail,com>
2023-01-17 07:54:27 -0300

    🍱 Incluindo componentes prontos do blade para envio de email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+</a>
+</td>
+</tr>
